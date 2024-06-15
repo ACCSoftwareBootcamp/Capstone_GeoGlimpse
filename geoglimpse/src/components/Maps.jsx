7c94e8a775445c2e041e7f2623d07b5acb9ca779
patrick <patrick.moriarty@g.austincc.edu>
2024-06-15 09:53:15 -0500

    Maps frontend and backend

--- geoglimpse/src/components/Maps.jsx
+++ geoglimpse/src/components/Maps.jsx
@@ -1,27 +1,78 @@
 import React, { useEffect, useState } from "react";
 
-function Maps({ query }) {
-  const [data, setData] = useState(null);
+function Maps() {
+  const [str, setStr] = useState(null);
 
-  useEffect(
-    function () {
-      if (query) {
-        fetch("/api/data?query=" + query)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(setData)
-          .catch(console.error);
-      }
-    },
-    [query]
-  );
+  let url = 'http://localhost:3002/GoogleMaps'
 
-  if (!data) return null;
+  let API = 'AIzaSyA1u2rNLif_SR6WBSbtufuAOGa-OQ4dL1A'
 
+  // useEffect(
+  //   function () {
+      
+  //       fetch(url)
+  //         .then(function (response) {
+  //           return response.json();
+  //         })
+  //         .then(function(data){
+  //           console.log(data)
+  //           setStr(data)
+  //           console.log(data)
+  //         })
+  //         .catch(function(error){
+  //           console.log(`There's an error and it says ${error}`)
+  //         });
+  //   },
+  //   []
+  // );
+
+  console.log(str)
   return (
     <div>
       <h2>Maps</h2>
+      <iframe
+        width="600"
+        height="450"
+        style={{ border:0 }}
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=${API}
+          &q=Space+Needle,Seattle+WA`}>
+      </iframe>
+      {/* <iframe
+        width="450"
+        height="250"
+        frameBorder="0" style={{ border:0 }}
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/directions?key=${API}
+        &destination=Telemark+Norway
+        &avoid=tolls|highways`}
+        allowFullScreen>
+      </iframe>
+      <iframe
+        width="600"
+        height="450"
+        style={{ border:0 }}
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/directions
+  ?key=${API}
+  &origin=Oslo+Norway
+  &destination=Telemark+Norway
+  &avoid=tolls|highways`}>
+      </iframe> */}
+      <iframe
+  width="450"
+  height="250"
+  frameBorder="0" style={{ border:0 }}
+  referrerPolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyA1u2rNLif_SR6WBSbtufuAOGa-OQ4dL1A&origin=Oslo+Norway
+  &destination=Telemark+Norway
+  &avoid=tolls|highways"
+  allowFullScreen>
+</iframe>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3002;
 
@@ -11,10 +12,18 @@ const { API_OpenWeather, API_GoogleMaps, API_News, API_RestfulCountries } = proc
 // Middleware to handle JSON requests
 app.use(express.json());
 
+// allow any browser to access our API
+app.use(cors())
+
 // Root route
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+app.get('/GoogleMaps', function(req, res){
+  console.log(API_GoogleMaps)
+  res.json(API_GoogleMaps)
+})
 
 // Start the server
 app.listen(port, () => {
