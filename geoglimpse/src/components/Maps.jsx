7d5701cb1998539f647221500e423aa75126c89e
patrick <patrick.moriarty@g.austincc.edu>
2024-07-06 09:54:42 -0500

    adding changes

--- geoglimpse/src/components/Maps.jsx
+++ geoglimpse/src/components/Maps.jsx
@@ -2,35 +2,57 @@ import React, { useEffect, useState } from "react";
 
 function Maps() {
   const [str, setStr] = useState(null);
+  const [place, setPlace] = useState('Seattle+WA');
 
   let url = 'http://localhost:3002/GoogleMaps'
 
   let API = 'AIzaSyA1u2rNLif_SR6WBSbtufuAOGa-OQ4dL1A'
   //test
 
-  useEffect(
-    function () {
-      
-        fetch(url)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function(data){
-            console.log(data)
-            setStr(data)
-            console.log(data)
-          })
-          .catch(function(error){
-            console.log(`There's an error and it says ${error}`)
-          });
-    },
-    []
-  );
+  const handleChange = (event) => {
+    // grab value from user input
+    let newStr = event.target.value;
+    console.log(newStr)
+    setPlace(newStr.replace(" ", "+"));
+  };
 
-  console.log(str)
+  // useEffect(
+  //   function () {
+      
+  //       fetch(url)
+  //         .then(function (response) {
+  //           return response.json();
+  //         })
+  //         .then(function(data){
+  //           console.log(data)
+  //           setStr(data)
+  //           console.log(data)
+  //         })
+  //         .catch(function(error){
+  //           console.log(`There's an error and it says ${error}`)
+  //         });
+  //   },
+  //   []
+  // );
+
+  // console.log(str)
   return (
     <div>
       <h2>Maps</h2>
+      <div>
+        <input 
+          type="text" 
+          placeholder="enter place"
+        />
+        <button>
+          onClick={() => {
+            handleChange()
+          }}
+        {/* onChange={handleChange} */}
+        value={place}
+        </button>
+      </div>
+        
       <iframe
         width="600"
         height="450"
@@ -38,8 +60,8 @@ function Maps() {
         loading="lazy"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
-        src={`https://www.google.com/maps/embed/v1/place?key=${str}
-          &q=Space+Needle,Seattle+WA`}>
+        src={`https://www.google.com/maps/embed/v1/place?key=${API}
+          &q=${place}`}>
       </iframe>
       {/* <iframe
         width="450"
