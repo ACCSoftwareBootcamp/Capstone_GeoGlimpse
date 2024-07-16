59463e77519f4eda2c605bfd7abdd275f0ef425d
Max Steward <maxwell.steward@g.austincc.edu>
2024-07-16 12:00:30 -0500

    small change to server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3002;
-// import process.env
 
-const dotenv = process.env;
-const { NEWS_KEY } = process.env;
+//Destructured the API Keys
+const { API_OpenWeather, API_GoogleMaps, API_News, API_RestfulCountries } =
+  process.env;
 
 // Middleware to handle JSON requests
 app.use(express.json());
@@ -13,21 +13,6 @@ app.use(express.json());
 // Root route
 app.get("/", (req, res) => {
   res.send("Server is running!");
-});
-
-// News endpoint
-app.get("/api/news", async (req, res) => {
-  const { countryNews } = req.query;
-  const apiKeyNews = NEWS_KEY;
-  const url = `https://newsapi.org/v2/top-headlines?country=${countryNews}&apiKey=${apiKeyNews}`;
-
-  try {
-    const response = await axios.get(url);
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    res.status(500).send("Error fetching news");
-  }
 });
 
 // Start the server
